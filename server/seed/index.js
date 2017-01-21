70adf385477c6df12be1d2d76708e4f0a9b4fd0f
Melanie Williams <melaniep518@gmail.com>
2017-01-21 16:15:34 -0500

    add index seed file

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,10 +1,14 @@
-const Location = require('../models').Location;
+const models = require('../models')
 
-Location.bulkCreate([
+models.User.bulkCreate([
+  {first_name:'Chanice', last_name:'Stlouis', user_name:'kobe24', e_mail:"chanicestlouis@aol.com", password:'pass123'},
+  {first_name:"Victor",last_name:"Amos",user_name:"Vamosa",e_mail:"victoramosjs@gmail.com",password:"pass123"},
+  {first_name:"Raybia",last_name:"Watson-Maiorana",user_name:"ormabe",e_mail:"ormabe@gmail.com",password:"pass123"}
+])
+.then(data => models.Location.bulkCreate([
   {location: "Home", street: "1621 Lexington Avenue, Apt. 2", city: "New York", state: "NY", zip: 10029, UserId: 1},
   {location: "Work", street: "31-00 47th Avenue", city: "Long Island City", state: "NY", zip: 11101, UserId: 1},
   {location: "Home", street: "1925 Glenwood Road, Apt. 4A", city: "Brooklyn", state: "NY", zip: 11230, UserId: 2},
   {location: "Work", street: "1470 Madison Avenue", city: "New York", state: "NY", zip: 10029, UserId: 2}
-])
-.then(data => console.log('successful'))
+]))
 .catch(error => console.log(error))
