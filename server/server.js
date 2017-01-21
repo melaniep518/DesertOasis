5b3c992ff693f4623fbd2dc573da243be9452ddf
Melanie Williams <melaniep518@gmail.com>
2017-01-21 12:06:37 -0500

    scaffold

--- server/server.js
+++ server/server.js
@@ -2,25 +2,17 @@ var express = require('express')
 var app = express()
 var bodyparser = require('body-parser')
 var path = require('path')
-var commentRouter =  require('./routes/api')
-var postRouter =  require('.backend/routes/api')
-var voteRouter =  require('.backend/routes/api')
 var db = require('./models')
 var router = express.Router();
-
-
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json())
 app.use(express.static('public'))
 
+// ********** Routes here **********
 
-app.use('/api/comment',commentRouter)
-// app.use('/api/vote',voteRouter)
-// app.use('/api/post',postRouter)
-// 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
+  res.sendFile(path.join(__dirname, '../client/views/index.html'))
 })
 
 db.sequelize.sync().then(function() {
